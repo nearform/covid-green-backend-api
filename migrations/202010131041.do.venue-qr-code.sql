780c7feebf01d1216aa6eb9291edcfc4d4acb51c
Chris Draycott-Wheatley <chriswheatley87@gmail.com>
2020-10-13 14:06:04 +0100

    fix(qrcode): tidy up data stored in db

--- migrations/202010131041.do.venue-qr-code.sql
+++ migrations/202010131041.do.venue-qr-code.sql
@@ -13,25 +13,24 @@ CREATE TABLE IF NOT EXISTS risky_venues (
   end_time TIMESTAMPTZ
 );
 
-CREATE TABLE IF NOT EXISTS qr_code (
-  id UUID PRIMARY KEY NOT NULL DEFAULT GEN_RANDOM_UUID(),
-  created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  receiver_email TEXT NOT NULL,
-  receiver_first_name TEXT NOT NULL,
-  receiver_surname TEXT NOT NULL,
-  location_type TEXT NOT NULL,
-  location_name TEXT NOT NULL,
-  location_address TEXT NOT NULL,
-  contact_email TEXT NOT NULL,
-  contact_phone TEXT NOT NULL
-);
-
 CREATE TABLE IF NOT EXISTS venue_types (
   id UUID PRIMARY KEY NOT NULL DEFAULT GEN_RANDOM_UUID(),
   name TEXT NOT NULL,
   details TEXT NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS qr_code (
+  id UUID PRIMARY KEY NOT NULL DEFAULT GEN_RANDOM_UUID(),
+  created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  venue_type TEXT NOT NULL,
+  venue_name TEXT NOT NULL,
+  venue_address TEXT NOT NULL,
+  contact_email TEXT NOT NULL,
+  contact_phone TEXT NOT NULL,
+  FOREIGN KEY(venue_type) REFERENCES venue_types(id)
+);
+
+DELETE FROM venue_types;
 INSERT INTO venue_types (name, details)
 VALUES
   ('Accommodation', 'For example, hotels, bed & breakfast accommodation and campsites'),
