705b3d58594a02fbd475d793eaf30c733641cef7
Paolo Chiodi <chiodi84@gmail.com>
2020-10-30 13:08:33 +0100

    store self-isolation and date and send it to lambda

--- migrations/202010271545.do.notices.sql
+++ migrations/202010271545.do.notices.sql
@@ -7,5 +7,6 @@ CREATE TYPE notice_status AS ENUM (
 CREATE TABLE IF NOT EXISTS notices (
   id UUID PRIMARY KEY NOT NULL DEFAULT GEN_RANDOM_UUID(),
   created_at DATE NOT NULL DEFAULT CURRENT_DATE,
+  self_isolation_end_date DATE NOT NULL,
   status notice_status NOT NULL DEFAULT 'available'
 );
